fe0f30df01ccb52ee38c2c93f801d51255af7ffb
Justin <JustinE@tlgcohort.com>
2024-07-09 11:43:04 -0500

    refactor using async/await to fetch photos

--- script.js
+++ script.js
@@ -41,10 +41,11 @@ document.addEventListener("DOMContentLoaded", () => {
     let location = document.getElementById("location").value.trim();
     let photoUrl;
     let description = document.getElementById("description").value.trim();
-    getPhoto();
-    //function for getting photo from unsplash api
+    //getPhoto();
+    fetchPhoto();
+    //function for getting photo from unsplash api with promises
     function getPhoto() {
-      let query = `&query=${destination}`;
+      let query = `&query=${destination}&query=${location}`;
       let endpoint = `${baseUrl}${client_id}${count}${query}`;
 
       fetch(endpoint)
@@ -71,6 +72,32 @@ document.addEventListener("DOMContentLoaded", () => {
           form.reset();
         })
         .catch((error) => console.error("Error fetching photo", error));
+    }
+    //using async await
+    async function fetchPhoto() {
+      let query = `&query=${destination}&query=${location}`;
+      let endpoint = `${baseUrl}${client_id}${count}${query}`;
+      try {
+        const response = await fetch(endpoint);
+        if (!response.ok) {
+          throw new Error(`Error fetching data: , ${response.data}`);
+        }
+        const data = await response.json();
+        console.log(data);
+        photoUrl = data[0].urls.regular;
+        console.log(photoUrl);
+        let newCard = { destination, location, photoUrl, description };
+        listArray.push(newCard);
+        localStorage.setItem("destinations", JSON.stringify(listArray));
+
+        let card = createCard(destination, location, photoUrl, description);
+        //ul.insertBefore(card, ul.firstChild);
+        ul.appendChild(card);
+        setListTitle();
+        form.reset();
+      } catch (error) {
+        console.log(`${error}`);
+      }
     }
   });
 
@@ -124,7 +151,7 @@ document.addEventListener("DOMContentLoaded", () => {
       if (newDestination !== null && newDestination !== "") {
         editedDestination = newDestination;
 
-        let query = `&query=${editedDestination}`;
+        let query = `&query=${editedDestination}&query=${editedLocation}`;
         let endpoint = `${baseUrl}${client_id}${count}${query}`;
         fetch(endpoint)
           .then((response) => {
